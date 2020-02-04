52b063b0d866a95ac618cb45bc7e158ea9f3399c
eklavyabhatia <eklavyabhatia@gmail.com>
2020-02-04 10:22:30 +0530

    added static files

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const app=express()
 app.set('view engine','ejs')
 app.set('views',path.join(__dirname,'views'))
 app.use(express.urlencoded({extended:true}))
+app.use(express.static('assets'))
 
 var contactsList=[
     {
